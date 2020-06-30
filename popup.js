4ca91ae80e49b7b0d45592c26097d09b52298682
Frank Norton <jfnorton32@gmail.com>
2020-06-30 03:35:11 -0400

    finish remove item & uniqueIDGenerator

--- popup.js
+++ popup.js
@@ -4,13 +4,40 @@
 
 // create storage array called "cartMaster"
 const cartMaster = [];
+
 // create function to build new cart items
-function cartItems (item, itemUrl, price) {
+function cartItems (item, url, price, id) {
   this.item = item,
-  this.url = itemUrl,
-  this.price = price
+  this.url = url,
+  this.price = price;
+  this.uniqueID = id
+}
+ 
+// removes item that contains a specific ID and shifts the rest of the array to fill the space
+function removeItem (id) {
+  for (let i = 0; i < cartMaster.length; i++) {
+    if (cartMaster[i].uniqueID === id) {
+      delete cartMaster[i];
+      for (i; i < cartMaster.length - 1; i++) {
+        cartMaster[i] = cartMaster [i + 1];
+      }
+      cartMaster.pop()
+      return;
+    }
+  }
 }
 
+// create a unique id for each list item.
+// random number between 0 and 1000
+// if same as a current ID, it will create another till it is unique
+function uniqueIDGenerator () {
+  let output = Math.round(Math.random() * 1000);
+  for (let i = 0; i < cartMaster.length; i++) {
+    if (cartMaster[i].uniqueID === output) {
+      output = uniqueIDGenerator();}
+  }
+  return output;
+}
 
 // ------------------Adding Comments---------------------------
 // eslint-disable-next-line func-names
@@ -33,10 +60,10 @@ window.onload = function () {
     // append the item to list
     document.querySelector('.list-group').appendChild(listItem);
 
-    const nextItem = new cartItem(item, itemUrl, price);
-    // alert{`${nextItem}`}
+    const nextItem = new cartItem(item, itemUrl, price, uniqueIDGenerator());
+    // alert(`${nextItem}`)
     cartMaster.push(nextItem);
-    // alert{`${cartMaster}`}
+    // alert(`${cartMaster}`)
   });
 };
 
